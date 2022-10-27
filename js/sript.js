c6ddc68dc3a92504db9ebd40ab1945db9d33af5a
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-27 13:23:44 +0000

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -39,5 +39,20 @@ function renderTasks (){
 
         const label = document.createElement("label");
         const input = document.createElement("input");
+        const span = document.createElement("span");
+        const content = document.createElement("div");
+        const actions = document.createElement("div");
+        const editButton = document.createElement("button");
+        const remove = document.createElement("button");
+
+        input.type = "checkbox";
+        input.checked = todo.done;
+        span.classList.add("category");
+
+        if(todo.category == "personal"){
+            span.classList.add("personal");
+        } else {
+            span.classList.add("work")
+        }
     })
 }
