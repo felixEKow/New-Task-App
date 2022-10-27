ec0adf4ef653eeaeaec71fec0e748d4eacd3f61f
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2022-10-27 11:21:56 +0000

    Update sript.js

--- js/sript.js
+++ js/sript.js
@@ -1,3 +1,9 @@
 window.addEventListener("load",() => {
     tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const nameInput = document.querySelector("#name");
+    const taskForm = document.querySelector("#newTodoForm");
+
+    const userName = localStorage.getItem("username") || "";
+
+    nameInput.value = userName;
 })
